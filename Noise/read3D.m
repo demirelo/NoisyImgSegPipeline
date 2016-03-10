b7ff88ddf1066851d8259da449a6bc9495999742
xeroquark <omerddd@gmail.com>
2016-03-10 10:24:30 +0100

    improved tif reading
    faster tif reading due to output variable initialization

--- Noise/read3D.m
+++ Noise/read3D.m
@@ -8,8 +8,9 @@ function A = read3D(fname)
 % Copyright: Omer Demirel (omerddd@gmail.com), University of Zurich, 2015
 
 info = imfinfo(fname);
-num_images = numel(info);
-
+num_images = numel(info); % get the number of slices in z
+xy = imread(fname,1);
+A = zeros(size(xy,1),size(xy,2),num_images);
 for k = 1:num_images
     A(:,:,k) = imread(fname, k, 'Info', info);
 end
